e080759f41791009e2ce1bcf6cf2436711b8d477
Gevorg Harutyunyan <gevharut@gmail.com>
2020-03-03 02:19:57 +0400

    Updates

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8e3ca8a69bdc6e37198a349a30988fc0.js"
+  "precache-manifest.03413badf9a3fee3e85ee4c3e28deac0.js"
 );
 
 self.addEventListener('message', (event) => {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fa3410de29ac0b1f3b37edd189d9594.js"
+  "/precache-manifest.80fa791d57b3e146919e92f95d9296e1.js"
 );
 
 self.addEventListener('message', (event) => {
